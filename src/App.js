9441f5e1ef07fe02fd7785d659e37b9f920c0a68
sunayana.shrestha <sthasunayana29@gmail.com>
2024-03-23 12:03:21 +0545

    update new images for care home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import Header from "./components/Header";
 import Dashboard from "./containers/Dashboard";
+import LoginPage from "./containers/LoginPage";
 import Footer from "./components/Footer";
 
 const App = () => {
@@ -10,8 +12,11 @@ const App = () => {
     <div className="App">
       <div className="container-fluid">
         <Header />
-        <Dashboard/>
-        <Footer/>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+        <Footer />
       </div>
     </div>
   );
