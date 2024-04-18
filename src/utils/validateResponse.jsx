f1a66a117cee0ba66d5be7ff2082a0f0e976e48f
sunayana <simathapa111@outlook.com>
2024-04-18 11:41:15 +0545

    update response validation

--- src/utils/validateResponse.jsx
+++ src/utils/validateResponse.jsx
@@ -1,9 +1,15 @@
 import { toast } from "react-toastify";
+import { deleteCookie } from "./setCookie";
 
 export const validateResponse = (response) => {
+  console.log("API RESPONSE===>", response)
   if (!response.ok) {
     if (response.status === 401 || response.status === 403) {
       toast.error("Authorization Access not granted for the user.");
+      deleteCookie("token");
+      deleteCookie("username");
+      localStorage.removeItem("book-detail");
+      toast.success("Logged out successfully!");
       window.location.replace("/login");
     } else {
       // Handle other errors
